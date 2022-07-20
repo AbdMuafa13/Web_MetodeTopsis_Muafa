e9ab0ed4637fca3d01311fc3616ee2233a339dec
AbdMuafa13 <abdmuafa@gmail.com>
2022-07-20 12:53:42 +0700

    first commit

---
+++ koneksilog.php
@@ -0,0 +1,8 @@
+<?php
+$conn = mysqli_connect("localhost","root","","data_hp");
+if (!$conn) {
+    echo"Gagal";
+} else{
+    echo"";
+}
+?>
---
+++ prosesadd.php
@@ -0,0 +1,26 @@
+<?php
+include 'koneksilog.php';
+$nama =$_POST['nama'];
+$username =$_POST['username'];
+$password =$_POST['password'];
+$role =$_POST['role'];
+
+
+
+   
+ 
+
+   
+
+     // query insert data
+    
+     
+     $cek = mysqli_query($conn, "insert into user values ('','$nama' ,'$username','$password','$role')");
+     if($cek) {
+        echo "<script>alert('Data berhasil ditambahkan'); window.location.href = 'daftaruser6.php'</script>";
+    }
+
+    
+     
+
+?>
